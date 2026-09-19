43b865955b56349ddf476261ce7bdfeabdc6d796
Paul Grayson <paul@pololu.com>
2026-09-18 18:51:21 -0700

    update gem version to 2.0.1

--- lib/nmatrix/version.rb
+++ lib/nmatrix/version.rb
@@ -29,11 +29,10 @@ class NMatrix
   module VERSION #:nodoc:
     MAJOR = 2
     MINOR = 0
-    TINY = 0
+    TINY = 1
     #PRE = "a"
 
     STRING = [MAJOR, MINOR, TINY].compact.join(".")
     #STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
 end
-
